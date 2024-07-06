dcf028b905e0ba1d372b6164f25519f1d2c5ef97
InDaZZ <f-k-87@list.ru>
2024-07-06 23:06:35 +0300

    ready

--- tz-app/src/Main/Main.jsx
+++ tz-app/src/Main/Main.jsx
@@ -9,40 +9,39 @@ import LorenzoImg from '../images/LORENZO.png';
 import ADALINAImg from '../images/ALDALINA.png';
 import ALEXANDERImg from '../images/ALEXANDER.png';
 import RUSHANAImg from '../images/RUSHANA.png';
-import { useEffect, useLayoutEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Main() {
-    const [hidenComment, setHidenComment] = useState(false);
+    console.log(window.innerWidth)
+    const [mobileVer, setMobileVer] = useState(false);
+    const resizeHandler = () => {
+        if (window.innerWidth > 420) {
+            setMobileVer(false)
+            return
+        }
+        else {
+            setMobileVer(true)
+        }
+    };
 
-    useLayoutEffect(() => {
-        const resizeHandler = () => {
-            if (window.innerWidth > 420) {
-                setHidenComment(false)
-                return
-            }
-            else {
-                setHidenComment(true)
-            }
-           
-        };
+    useEffect(() => {
         window.addEventListener('resize', resizeHandler);
-        resizeHandler()
         return () => window.removeEventListener('resize', resizeHandler);
     }, [])
 
     return (
         <main className='main'>
-            <h1 className='main__title'>DO YOU WANT TO LEARN MORE ABOUT CRYPTOCURRENC{hidenComment && <br></br>}IES <span className='main__title-gradient'>QUICKLY AND EASILY ?</span></h1>
+            <h1 className='main__title'>DO YOU WANT TO LEARN MORE ABOUT CRYPTOCURRENC{mobileVer && <br></br>}IES <span className='main__title-gradient'>QUICKLY AND EASILY ?</span></h1>
             <BackgroundValute valuteIcon={Bitcoin} valuteType={'bitcoin'}></BackgroundValute>
             <BackgroundValute valuteIcon={Leticoin} valuteType={'litecoin'}></BackgroundValute>
             <BackgroundValute valuteIcon={Ethereum} valuteType={'ethereum'}></BackgroundValute>
-            {!hidenComment && <Comment userImage={LorenzoImg} userMail={'@lorenzoo'} userName={'LORENZO'} massage={'Amazing Telegram bot! Provides real-time crypto prices and news'} hidenComment={hidenComment}></Comment>}
+            {(window.innerWidth > 420) && <Comment userImage={LorenzoImg} userMail={'@lorenzoo'} userName={'LORENZO'} massage={'Amazing Telegram bot! Provides real-time crypto prices and news'} hidenComment={mobileVer}></Comment>}
 
-            {!hidenComment && <Comment userImage={ADALINAImg} userMail={'@ada'} userName={'ADALINA'} massage={'Must-have bot for crypto traders. Accurate signals and analysis'} hidenComment={hidenComment}></Comment>}
+            {(window.innerWidth > 420) && <Comment userImage={ADALINAImg} userMail={'@ada'} userName={'ADALINA'} massage={'Must-have bot for crypto traders. Accurate signals and analysis'} hidenComment={mobileVer}></Comment>}
 
-            {!hidenComment && <Comment userImage={ALEXANDERImg} userMail={'@alex_x'} userName={'ALEXANDER'} massage={'Superb cryptocurrency bot! Quick updates and reliable data'} hidenComment={hidenComment}></Comment>}
+            {(window.innerWidth > 420) && <Comment userImage={ALEXANDERImg} userMail={'@alex_x'} userName={'ALEXANDER'} massage={'Superb cryptocurrency bot! Quick updates and reliable data'} hidenComment={mobileVer}></Comment>}
 
-            {!hidenComment && <Comment userImage={RUSHANAImg} userMail={'@Hana'} userName={'RUSHANA'} massage={'Efficient and user-friendly bot. Simplifies crypto trading tasks'} hidenComment={hidenComment}></Comment>}
+            {(window.innerWidth > 420) && <Comment userImage={RUSHANAImg} userMail={'@Hana'} userName={'RUSHANA'} massage={'Efficient and user-friendly bot. Simplifies crypto trading tasks'} hidenComment={mobileVer}></Comment>}
             <Subscribe></Subscribe>
         </main>
     )
